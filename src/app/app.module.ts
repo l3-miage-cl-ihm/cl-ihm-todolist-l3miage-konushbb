8e945b700327c8c15bdcfd9eb415f61387bc28e0
l3miage-konushbb <begimai.konushbaeva@mail.ru>
2022-03-25 10:06:39 +0100

    Init a firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { AngularFireModule } from '@angular/fire/compat';
+import { environment } from '../environments/environment';
 
 import { AppComponent } from './app.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
@@ -14,7 +16,9 @@ import { TodoItemComponent } from './todo-item/todo-item.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AngularFireModule.initializeApp(environment.firebase)
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,9 +2,21 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+import { FirebaseApp, FIREBASE_OPTIONS } from "@angular/fire/compat";
+
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAKpT7Rk01ho_aR76PaLmrN_MvTo2fbIkU",
+    authDomain: "todolist-2f842.firebaseapp.com",
+    projectId: "todolist-2f842",
+    storageBucket: "todolist-2f842.appspot.com",
+    messagingSenderId: "809228970383",
+    appId: "1:809228970383:web:5349fc58a51f932a6203fa",
+    measurementId: "G-HQMLLFEMCC"
+  }
 };
+
 
 /*
  * For easier debugging in development mode, you can import the following file
